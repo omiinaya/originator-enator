287bf2a4b52644179249feacf056ed19deb4bab5
Omar Minaya <omiinaya@gmail.com>
2021-06-16 23:55:41 -0400

    registering power plans if they dont exist

--- main.js
+++ main.js
@@ -120,10 +120,21 @@ function setMonitorTimeout() {
 }
 
 function setPowerCfg(a) {
-  //if there is no high setting, use ultimate
+  //if there is no high setting, register it
   if (!getPowerGUID(a)) {
-    return execSync('powercfg /setactive ' + getPowerGUID('Ultimate'))
+    registerPowerPlan(a) 
   } else {
     return execSync('powercfg /setactive ' + getPowerGUID(a))
+  }
+}
+
+function registerPowerPlan(a) {
+  if (a === "High") {
+    execSync('powercfg -duplicatescheme 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c')
+    setPowerCfg(a)
+  } else {
+    //unnecessary else with GUID for Ultimate
+    execSync('powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61')
+    setPowerCfg(a)
   }
 }
