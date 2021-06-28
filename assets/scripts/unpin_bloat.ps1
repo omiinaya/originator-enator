c99b0ea55ded61a522d979a0e158f099f098dc1c
Omar Minaya <omiinaya@gmail.com>
2021-06-28 12:30:16 -0400

    find another way to elevate

--- assets/scripts/unpin_bloat.ps1
+++ assets/scripts/unpin_bloat.ps1
@@ -1,5 +1,9 @@
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -force
+
 $appname = "Microsoft Store"
  ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'Unpin from taskbar'} | %{$_.DoIt(); $exec = $true}
 
  $appname = "Xbox"
  ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'Unpin from taskbar'} | %{$_.DoIt(); $exec = $true}
+
+ Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Default -force
